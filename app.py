b1296b6fa3355267e8e98004d476a9f712020968
unknown <ivan@CienciayDatos.es>
2020-09-08 12:03:31 +0200

    version 1

---
+++ app.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Stocks from Yahoo Fianance
+
+Show Closing Price Selected!
+
+""")
+
+tickerSymbol = st.text_input("Select ticker: ", 'GOOGL') # in python variable=input() # example input:'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf =  tickerData.history(period='1d',start='2010-5-31',end= '2022-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+st.subheader('ivan@cienciaydatos.es')
